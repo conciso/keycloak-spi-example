e9a823e37e72a860c1ed0f7a0a5f80f8b2199382
Sven-Torben Janus <sven-torben.janus@conciso.de>
2020-07-06 19:50:47 +0200

    Demo - Step 3: Activate logging

--- src/main/java/com/github/conciso/keycloak/email/EmailPrefixSenderProvider.java
+++ src/main/java/com/github/conciso/keycloak/email/EmailPrefixSenderProvider.java
@@ -1,5 +1,6 @@
 package com.github.conciso.keycloak.email;
 
+import org.jboss.logging.Logger;
 import org.keycloak.email.EmailSenderProvider;
 import org.keycloak.models.UserModel;
 
@@ -7,10 +8,13 @@ import java.util.Map;
 
 public class EmailPrefixSenderProvider implements EmailSenderProvider {
 
+  private static final Logger logger = Logger.getLogger(EmailPrefixSenderProvider.class);
+
   EmailPrefixSenderProvider() {
   }
 
   public void send(Map<String, String> config, UserModel user, String subject, String textBody, String htmlBody) {
+    logger.infov("Sending email");
   }
 
   public void close() {
